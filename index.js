a2f3b2224bf7b6e5b74da13832124747cafc84b1
Ingvar Stepanyan <me@rreverser.com>
2014-10-16 16:26:21 +0300

    Added global storage for registered parsers.

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 var Rx = require('rx');
 var extname = require('path').extname;
 
-var parsers = {};
+// Yes, this needs to be single global storage for any versions of aster-parse installed.
+var parsers = global['aster-parse'] || (global['aster-parse'] = {});
 
 function parse(options) {
 	return function (files) {
